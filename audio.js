44677ba86d2c5930aea1451559e505195d872bbb
Fhy40 <arjunsundram@gmail.com>
2014-07-06 02:57:58 +0800

    Create audio.js
    Audiozzz

---
+++ audio.js
@@ -0,0 +1,43 @@
+var interventionFire = new Audio("sounds/intervention_fire.mp3");
+var smokeWeed = new Audio("sounds/smoke_weed.mp3");
+var hitmarker;
+
+if(navigator.userAgent.toLowerCase().indexOf('firefox') < 0) {
+     hitmarker = new Audio("sounds/hitmarker.mp3");
+} else {
+	hitmarker = new Audio("sounds/hitmarker_alt.mp3");
+}
+
+var hornSounds = 
+	[
+	new Audio("sounds/horn/horn_c.mp3"),
+	new Audio("sounds/horn/horn_c_sharp.mp3"),
+	new Audio("sounds/horn/horn_d.mp3"),
+	new Audio("sounds/horn/horn_d_sharp.mp3"),
+	new Audio("sounds/horn/horn_e.mp3"),
+	new Audio("sounds/horn/horn_f.mp3"),
+	new Audio("sounds/horn/horn_f_sharp.mp3"),
+	new Audio("sounds/horn/horn_g.mp3"),
+	new Audio("sounds/horn/horn_g_sharp.mp3"),
+	new Audio("sounds/horn/horn_a.mp3"),
+	new Audio("sounds/horn/horn_a_sharp.mp3"),
+	new Audio("sounds/horn/horn_b.mp3"),
+	new Audio("sounds/horn/horn_c_high.mp3")
+	];
+
+var weedSounds = 
+	[
+	new Audio("sounds/weed/weed_c.mp3"),
+	new Audio("sounds/weed/weed_c_sharp.mp3"),
+	new Audio("sounds/weed/weed_d.mp3"),
+	new Audio("sounds/weed/weed_d_sharp.mp3"),
+	new Audio("sounds/weed/weed_e.mp3"),
+	new Audio("sounds/weed/weed_f.mp3"),
+	new Audio("sounds/weed/weed_f_sharp.mp3"),
+	new Audio("sounds/weed/weed_g.mp3"),
+	new Audio("sounds/weed/weed_g_sharp.mp3"),
+	new Audio("sounds/weed/weed_a.mp3"),
+	new Audio("sounds/weed/weed_a_sharp.mp3"),
+	new Audio("sounds/weed/weed_b.mp3"),
+	new Audio("sounds/weed/weed_c_high.mp3")
+	];
